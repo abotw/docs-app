ed6a11237a5e6c2cceee2b0dd173238cfd9baa44
abotw <>
2026-01-05 16:25:48 +0800

    update

--- __TODO/MKDocs/NotebookTemplate/site/Courses/40823-Java/2025-06-19/PR6.java
+++
@@ -1,9 +0,0 @@
-public class PR6 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
-        }
-    }
-}
--- __TODO/MKDocs/NotebookTemplate/site/Courses/4088-PD/GUI/GUIProgrammingP1/ShowInput.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class ShowInput {
-    public static void main(String[] args) {
-        String str = JOptionPane.showInputDialog("Type some text: ");
-        JOptionPane.showMessageDialog(null, "str is: " + str);
-    }
-}
